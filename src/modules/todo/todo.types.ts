8115ca0070a407c92e1b99eba0c6b3b3655d6a16
AkkalDhami <dhamiakkal21@gmail.com>
2026-01-30 16:25:44 +0545

    feat: add todo types

--- src/modules/todo/todo.types.ts
+++ src/modules/todo/todo.types.ts
@@ -10,3 +10,17 @@ export interface ITodo {
   createdAt?: Date | null;
   updatedAt?: Date | null;
 }
+
+export interface IPagination {
+  page: number;
+  limit: number;
+  total: number;
+  pages: number;
+}
+
+export type TodoFilterType = {
+  page?: string;
+  limit?: string;
+  search?: string;
+  sortBy?: "asc" | "desc"
+};
