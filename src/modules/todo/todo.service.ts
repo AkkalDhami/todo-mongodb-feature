f12494cb3bb14859577b20f4c5e032d12fb59d74
AkkalDhami <dhamiakkal21@gmail.com>
2026-01-30 16:25:32 +0545

    feat: add todo services

--- src/modules/todo/todo.service.ts
+++ src/modules/todo/todo.service.ts
@@ -1,7 +1,8 @@
 import { ApiError } from "@/shared/utils/api-error";
 import Todo from "./todo.model";
-import { ITodo } from "./todo.types";
+import { IPagination, ITodo, TodoFilterType } from "./todo.types";
 import { UpdateTodoInputType } from "./todo.validation";
+import mongoose from "mongoose";
 
 export class TodoService {
   static async createTodo({
@@ -18,9 +19,57 @@ export class TodoService {
     });
   }
 
-  static async getTodos(userId: string): Promise<ITodo[]> {
-    const todos = await Todo.find({ userId });
-    return todos;
+  static async getTodos(userId: string, filters: TodoFilterType):
+    Promise<{
+      todos: ITodo[],
+      pagination: IPagination
+    }> {
+
+    const page = Number(filters.page) || 1;
+    const limit = Number(filters.limit) || 10;
+    const search = filters.search || "";
+    const sortBy = filters.sortBy || "desc";
+
+    const skip = (page - 1) * limit;
+
+    const todos = await Todo.aggregate([
+      {
+        $match: {
+          userId: new mongoose.Types.ObjectId(userId),
+          $or: [
+            { title: { $regex: search, $options: "i" } },
+            { description: { $regex: search, $options: "i" } }
+          ]
+        }
+      },
+      {
+        $sort: {
+          createdAt: sortBy === "asc" ? 1 : -1
+        }
+      },
+      {
+        $skip: skip
+
+      },
+      {
+        $limit: limit
+
+      },
+
+    ])
+
+    const total = await Todo.countDocuments({ userId });
+    const pages = Math.ceil(total / limit);
+
+    return {
+      todos,
+      pagination: {
+        page,
+        limit,
+        total,
+        pages,
+      },
+    };
   }
 
   static async getTodoById(todoId: string): Promise<ITodo | null> {
